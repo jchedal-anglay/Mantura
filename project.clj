52d51cf572ddb9a0c6b3be4ae27b13e27fb87f0e
Julien Chedal-Anglay <chedala.julien@gmail.com>
2019-08-12 03:46:42 +0200

    Started test port to test.check, added tests for success/return

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject mantura "0.1.0"
   :description "A parser combinator library for Clojure"
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/test.check "0.9.0"]]
   :repl-options {:init-ns mantura.core})
