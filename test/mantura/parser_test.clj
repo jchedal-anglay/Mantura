38dc3ab7a44742460184e95f1bb8734537596a60
Julien CHEDAL-ANGLAY <jchedal-@e1r8p3.42.fr>
2019-08-19 10:49:23 +0200

    Added test for parser.take

--- test/mantura/parser_test.clj
+++ test/mantura/parser_test.clj
@@ -32,3 +32,25 @@
       (if (and (not-empty input) (= (first input) tok))
         {:state :success :content tok :remaining (rest input)}
         {:state :failure}))))
+
+;; TODO: test-tokens
+
+(defspec test-take
+  20
+  (prop/for-all
+   [[input size]
+    (gen/let
+        [input (gen/list gen/any)
+         size (gen/fmap #(if (= 0 (count input)) 0 (+ 1 (mod % (count input)))) gen/int)]
+      [input size])]
+   (let [size-too-big (+ 1 (count input))]
+     (and
+
+      ;; Check (take 0)'s idempotence
+      (= (core/run (parser/take 0) input)
+         (core/run (parser/take 0) (:remaining (core/run (parser/take 0) input))))
+
+      (= (core/run (parser/take size-too-big) input)
+         {:state :failure})
+      (= (core/run (parser/take size) input)
+         {:state :success :content (take size input) :remaining (drop size input)})))))
